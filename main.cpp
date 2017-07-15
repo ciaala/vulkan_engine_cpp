23294194661a5a7ec8c20962d125a32910dd9eeb
ciaala <francesco.fiduccia@gmail.com>
2017-07-15 19:16:38 +0200

    adds the vulkan library and headers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vulkan/vulkan.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Vulkan Version: " << VK_HEADER_VERSION << std::endl;
     return 0;
 }
