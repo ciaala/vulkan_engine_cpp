6a86952452923a5dd7ae76b2d1a5f29603c0900a
ciaala <francesco.fiduccia@gmail.com>
2017-07-16 00:48:38 +0200

    add xcb library to build system

--- engine/include/Renderer.hpp
+++ engine/include/Renderer.hpp
@@ -4,12 +4,25 @@
 
 #ifndef VULKAN_ENGINE_CPP_RENDERER_HPP
 #define VULKAN_ENGINE_CPP_RENDERER_HPP
+#define VK_USE_PLATFORM_XCB_KHR
+#include <xcb/xcb.h>
+#include <X11/Xutil.h>
 
 namespace vlk {
     class Renderer {
+
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+        xcb_window_t xcb_window;
+        xcb_screen_t *screen;
+        xcb_connection_t *connection;
+        xcb_intern_atom_reply_t *atom_wm_delete_window;
+#endif
     public:
         Renderer();
+
         void init_device();
+
+        void initWindowLibrary();
     };
 }
 
--- engine/src/Renderer.cpp
+++ engine/src/Renderer.cpp
@@ -2,12 +2,37 @@
 // Created by crypt on 15/07/17.
 //
 
+#include <iostream>
 #include "../include/Renderer.hpp"
 namespace vlk {
     Renderer::Renderer() {
+        this->initWindowLibrary();
         this->init_device();
     }
+    void Renderer::initWindowLibrary() {
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+        const xcb_setup_t *setup;
+        xcb_screen_iterator_t iter;
+        int scr;
 
+        connection = xcb_connect(nullptr, &scr);
+        if (xcb_connection_has_error(connection) > 0) {
+            printf(
+                "Cannot find a compatible Vulkan installable client driver "
+                "(ICD).\nExiting ...\n");
+            fflush(stdout);
+            exit(1);
+        }
+
+        setup = xcb_get_setup(connection);
+        iter = xcb_setup_roots_iterator(setup);
+        while (scr-- > 0) xcb_screen_next(&iter);
+
+        screen = iter.data;
+#else
+        std::cout << "Unable to idenfiy window library. Supported {xcb}" << std::endl;
+#endif
+        }
     void Renderer::init_device() {
 
     }
